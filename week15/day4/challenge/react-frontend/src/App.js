a5c06e90eb0ce618e370b96e62a554686e6f0c4d
Mika Bibas <mikabibas@gmail.com>
2022-07-18 15:49:15 +0300

    week15 day4 challenge

---
+++ week15/day4/challenge/react-frontend/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { message: "Hi", submitMessage: "" };
+  }
+  async componentDidMount() {
+    const response = await fetch("/api/hello", {
+      method: "GET",
+      mode: "no-cors",
+      headers: { "content-type": "text/plain" },
+    });
+    const body = await response.text();
+    this.setState({ message: body });
+  }
+  submitText = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const response = await fetch("/api/world", {
+      method: "POST",
+      mode: "no-cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ post: data.get("userInput") }),
+    });
+    const res = await response.text();
+    this.setState({ submitMessage: res });
+  };
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <header>{this.state.message}</header>
+        <h2 style={{ fontWeight: "bold" }}>Post to Server:</h2>
+        <form onSubmit={this.submitText}>
+          <input type={"text"} name="userInput" />
+          <button>Submit</button>
+        </form>
+        <h1>{this.state.submitMessage}</h1>
+      </div>
+    );
+  }
+}
+export default App;
